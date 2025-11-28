5c235c26e0577e296948e540ee356c5eca86410d
effie882 <effiemorara@gmail.com>
2025-11-28 10:32:06 +0300

    created aclass anogram that compares two words

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word: str):
+    
+        self.word = word
+
+    @staticmethod
+    def are_anagrams(str1: str, str2: str) -> bool:
+
+        str1_cleaned = str1.replace(" ", "").lower()
+        str2_cleaned = str2.replace(" ", "").lower()
+
+    
+        return sorted(str1_cleaned) == sorted(str2_cleaned)
+
+    def match(self, word_list):
+        """Find all anagrams of the stored word in the given list."""
+        return [word for word in word_list if self.are_anagrams(self.word, word)]
